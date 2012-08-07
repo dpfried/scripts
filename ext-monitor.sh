df15bfedc14665be05eb8b480b462a3799ae413d
Daniel Fried <dfried@email.arizona.edu>
2012-08-06 23:20:16 -0700

    added ext-monitor to control external monitor

---
+++ ext-monitor.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+#xrandr --newmode "1280x800_60.00" 83.50 1280 1352 1480 1680 800 803 809 8 31 -hsync +vsync
+#xrandr --addmode LVDS1 1280x800_60.00
+#xrandr --newmode "1280x1024_60.00" 109.00 1280 1368 1469 1712 1024 1027 1034 1063 -hsync +vsync
+#xrandr --addmode VGA1 1280x1024_60.00
+xrandr --output VGA1 --mode 1290x1024 --left-of LVDS1
+exec "~/scripts/wallpaper.sh"
+
+exit 0
+
